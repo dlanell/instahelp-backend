6c52298f70663a4273ad1f19e8c25ad4f1421dff
Mike Sanders <sanders.michael.j@gmail.com>
2020-03-25 14:33:48 -0400

    Initial commit

---
+++ src/main/java/com/instahelp/backend/InstahelpApplication.java
@@ -0,0 +1,13 @@
+package com.instahelp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstahelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstahelpApplication.class, args);
+	}
+
+}
